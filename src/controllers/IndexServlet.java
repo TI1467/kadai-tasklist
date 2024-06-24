39acd306dad619ca8f20bbea1844a7d337956953
e495 <e495@192.168.10.104>
2024-06-24 16:55:31 +0900

    Index/NewServletの修正

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.io.IOException;
 
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +13,8 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import models.Task;
 import utils.DBUtil;
+
+import javax.servlet.RequestDispatcher;
 
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
@@ -25,9 +28,13 @@ public class IndexServlet extends HttpServlet {
         EntityManager em = DBUtil.createEntityManager();
 
         List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
 
         em.close();
+
+        request.setAttribute("tasks", tasks);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
     }
 
 }
